c9138fe798a15a5460628f924867e5ed65c8f67b
Kayla Iacovino <kaylaiacovino@gmail.com>
2020-07-02 12:13:29 -0500

    Update anvil server (testing)

--- anvil_server.py
+++ anvil_server.py
@@ -1,3 +1,11 @@
+import VESIcal as v
 import anvil.server
 
 anvil.server.connect("KYKUMVNXIZ23YPRPDDGL5DM6-E4XIIJ7V23RSDNS5")
+
+@anvil.server.callable
+def import_ExcelFile(file):
+	with anvil.media.TempFile(file) as filename:
+		myfile = v.ExcelFile(filename)
+
+	return(str(filename) +" has been loaded!")
