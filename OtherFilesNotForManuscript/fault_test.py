7c2f7ba7191efeb2db4c62b4becb793ac96bd3dc
Kayla Iacovino <kaylaiacovino@gmail.com>
2020-09-08 13:09:53 -0500

    Cleaning up files that don't need to be on the repo.

--- OtherFilesNotForManuscript/fault_test.py
+++
@@ -1,7 +0,0 @@
-import faulthandler; faulthandler.enable() #to handle segmentation faults thrown by MELTS/thermoengine
-import VESIcal as v 
-
-myfile = v.ExcelFile('Statia_MI_Cooper_et_al_5.xlsx')
-
-satPs = myfile.calculate_saturation_pressure(temperature="Temp")
-print(satPs)
