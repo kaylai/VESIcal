d56d80631b13a7974a4ccf989cbd833d6010e024
Kayla Iacovino <kaylaiacovino@gmail.com>
2020-10-19 16:17:53 -0500

    Fix bug in anvil server that would cause file not to upload properly

--- anvil_server.py
+++ anvil_server.py
@@ -28,7 +28,7 @@ def import_ExcelFile(file):
 	with anvil.media.TempFile(file) as filename:
 		gMyFile = v.ExcelFile(filename)
 
-	gMyFile.data = gMyFile.data.reset_index().set_index('Label', drop=False) #turn Label column into normal column so that it's not lost
+	#gMyFile.data = gMyFile.data.reset_index().set_index('Label', drop=False) #turn Label column into normal column so that it's not lost
 	add_df_to_anvil_table(gMyFile.data, app_tables.user_data)
 
 	return("Your file has been loaded!")
