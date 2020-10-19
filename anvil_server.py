9e67973b2622fb266e105567a4a76049886aaaa8
Kayla Iacovino <captainkayla@gmail.com>
2020-10-19 15:48:37 -0500

    Update anvil server with working functions (restore lost work)

--- anvil_server.py
+++ anvil_server.py
@@ -4,11 +4,19 @@ import anvil.media
 import pandas as pd
 from anvil.tables import app_tables
 
-oxides = ['SiO2', 'TiO2', 'Al2O3', 'Fe2O3', 'Cr2O3', 'FeO', 'MnO', 'MgO', 'NiO', 'CoO', 'CaO', 'Na2O', 'K2O', 'P2O5',
-		  'H2O', 'CO2']
+oxides = v.oxides
 
 anvil.server.connect("KYKUMVNXIZ23YPRPDDGL5DM6-E4XIIJ7V23RSDNS5")
 app_tables.user_data.delete_all_rows()
+
+gMyFile = None
+
+def add_df_to_anvil_table(dataframe, anvil_table):
+	for d in dataframe.to_dict(orient="records"):
+		  # d is now a dict of {columnname -> value} for this row
+		  # We use Python's **kwargs syntax to pass the whole dict as
+		  # keyword arguments
+		  anvil_table.add_row(**d)
 
 @anvil.server.callable
 def get_user_data():
@@ -16,45 +24,63 @@ def get_user_data():
 
 @anvil.server.callable
 def import_ExcelFile(file):
+	global gMyFile
 	with anvil.media.TempFile(file) as filename:
-		myfile = v.ExcelFile(filename)
+		gMyFile = v.ExcelFile(filename)
 
-	#this isn't working :(
-	myfile.data = myfile.data.reset_index().set_index('Label', drop=False) #turn Label column into normal column so that it's not lost
-
-	for d in myfile.data.to_dict(orient="records"):
-		# d is now a dict of {columnname -> value} for this row
-		# We use Python's **kwargs syntax to pass the whole dict as
-		# keyword arguments
-		app_tables.user_data.add_row(**d)
+	gMyFile.data = gMyFile.data.reset_index().set_index('Label', drop=False) #turn Label column into normal column so that it's not lost
+	add_df_to_anvil_table(gMyFile.data, app_tables.user_data)
 
 	return("Your file has been loaded!")
 
 @anvil.server.callable
-def anvil_calculate_saturation_pressure(model, **kwargs):
-	user_data = app_tables.user_data.search()
-
-	columnnames = ['Label','SiO2', 'TiO2', 'Al2O3', 'Fe2O3', 'Cr2O3', 'FeO', 'MnO', 'MgO', 'NiO', 'CoO', 'CaO', 'Na2O', 'K2O', 'P2O5',
-		  'H2O', 'CO2']
-
-	myfile_data = pd.DataFrame({oxide: row[oxide] for oxide in oxides} for row in user_data)
-
-	iterno = 0
-	for index, row in myfile_data.iterrows():
-		iterno+=1
-		result = v.calculate_saturation_pressure(sample=row, temperature=1000, model=model).result
-		app_tables.output_data.add_row(Name="Sample No."+str(iterno),
-										SaturationP_bars=result)
-
-	return("Done!")
+def anvil_calculate_saturation_pressure(temperature, model, **kwargs):
+	global gMyFile
+	global gSatP_results_df
+	gSatP_results_df = gMyFile.calculate_saturation_pressure(temperature=temperature, model=model)
+	return("Successfully calculated saturation pressures")
 
 @anvil.server.callable
-def pull_data():
-	output_data = app_tables.output_data.search()
+def anvil_calculate_dissolved_volatiles(temperature, pressure, X_fluid, model, **kwargs):
+	global gMyFile
+	global gDissolved_results_df
+	gDissolved_results_df = gMyFile.calculate_dissolved_volatiles(temperature=temperature, 
+																pressure=pressure,
+																X_fluid=X_fluid,
+																model=model)
+	return("Successfully calculated dissolved volatile concentrations")
 
-	results_df = pd.DataFrame({'Name': row['Name'], 'SaturationP_bars_VESIcal': row['SaturationP_bars']} for row in output_data)
+@anvil.server.callable
+def anvil_calculate_equilibrium_fluid_comp(temperature, pressure, model, **kwargs):
+	global gMyFile
+	global gEqfluid_results_df
+	gEqfluid_results_df = gMyFile.calculate_equilibrium_fluid_comp(temperature=temperature, 
+																pressure=pressure,
+																model=model)
+	return("Successfully calculated equilibrium fluid compositions")
 
-	with pd.ExcelWriter('test_output.xlsx') as writer:
-			return results_df.to_excel(writer)
+@anvil.server.callable
+def download_satPs():
+	global gSatP_results_df
+	with pd.ExcelWriter('VESIcal_satPs.xlsx') as writer:
+		gSatP_results_df.to_excel(writer)
+
+	return anvil.media.from_file("./VESIcal_satPs.xlsx")
+
+@anvil.server.callable
+def download_dissolved():
+	global gDissolved_results_df
+	with pd.ExcelWriter('VESIcal_dissolved.xlsx') as writer:
+		gDissolved_results_df.to_excel(writer)
+
+	return anvil.media.from_file("./VESIcal_dissolved.xlsx")
+
+@anvil.server.callable
+def download_eqfluid():
+	global gEqfluid_results_df
+	with pd.ExcelWriter('VESIcal_eqfluid.xlsx') as writer:
+		gEqfluid_results_df.to_excel(writer)
+
+	return anvil.media.from_file("./VESIcal_eqfluid.xlsx")
 
 anvil.server.wait_forever()
