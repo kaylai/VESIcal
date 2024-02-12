1a175befffcba9f78074ce6ea238ba4a02bec82a
Kayla Iacovino <kaylaiacovino@gmail.com>
2024-02-12 17:55:01 -0600

    Update dataframe handling for pandas 2.0, update to version 1.2.6 for pip.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="VESIcal",
-    version="1.2.5",
+    version="1.2.6",
     author="Kayla Iacovino, Simon Matthews, Penny Wieser",
     author_email="kaylaiacovino@gmail.com",
     description=("A generalized python library for calculating and plotting various things "
