917d410a7acb88f6ce103a0d6a3edc1ea2914cf3
Kayla Iacovino <kaylaiacovino@gmail.com>
2024-03-29 15:24:00 -0500

    Create new distribution files for version 1.2.8 and push to pip.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="VESIcal",
-    version="1.2.7",
+    version="1.2.8",
     author="Kayla Iacovino, Simon Matthews, Penny Wieser",
     author_email="kaylaiacovino@gmail.com",
     description=("A generalized python library for calculating and plotting various things "
